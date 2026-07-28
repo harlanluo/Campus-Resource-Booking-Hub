22e9a24e1e166ca1333cbe9f81b9c37e00a1e04b
Harlan <181625686+harlanluo@users.noreply.github.com>
2026-07-28 22:06:38 +1200

    chore: setup Spring Boot 3 Maven project configuration and H2 database

---
+++ src/main/java/com/campusbooking/CampusBookingHubApplication.java
@@ -0,0 +1,12 @@
+package com.campusbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusBookingHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusBookingHubApplication.class, args);
+    }
+}
